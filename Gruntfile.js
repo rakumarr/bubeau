094123e12d672247319ad676d15446ff795ca0ce
HR <rajesh.msse@gmail.com>
2016-09-29 10:31:40 +0530

    thrid commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,7 @@ module.exports = function (grunt) {
       options: {
         port: 8589,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '192.168.0.105',
+        hostname: 'localhost',
         livereload: 35729
       },
       livereload: {
