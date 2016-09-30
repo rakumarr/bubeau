d16f77140d4edad3870dcae2bbaacb06c92f7463
HR <rajesh.msse@gmail.com>
2016-09-30 23:42:19 +0530

    To Deploy in Heroku

---
+++ web.js
@@ -0,0 +1,11 @@
+/**
+ * Created by hr on 30/9/16.
+ */
+
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
+
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
